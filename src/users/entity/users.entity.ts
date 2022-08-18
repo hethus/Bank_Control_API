9f697d9a938620517f151f3fb569854586f17ca5
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-08-18 17:53:38 -0300

    feature: created the users resource

---
+++ src/users/entity/users.entity.ts
@@ -0,0 +1,11 @@
+export class User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  lastMessage: string;
+  lastMessageAt: Date;
+  messagesCreateds: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
