476384d3f9c28dab87c807bb94b582017af7ab14
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-08-18 13:21:18 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.set('trust proxy', 1);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('E Winhout Mail API')
+    .setDescription(
+      "E Winhout Mail API is a RESTful API game for sending messages to the database and receiving messages from the database, and also you can't see who sent the message to you.",
+    )
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT || 8000);
+}
+bootstrap();
